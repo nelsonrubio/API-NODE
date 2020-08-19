f89950c0c43ddf196521fa926137595c68dccbe4
nelsonrubio <nelsonrubio20@gmail.com>
2020-08-18 23:05:49 -0400

    subiendo cambios

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const axios = require("axios");
-//const { response } = require("express");
 const app = express();
-//let requetItunes = require('ApiItunes');
+
 
 app.get('/search/:nombre', function(req, res) {
-    let one = "https://itunes.apple.com/search?term=" + req.params.nombre + "&limit=25";
-    let two = "https://api.tvmaze.com/singlesearch/shows?q=" + req.params.nombre;
-    const requestOne = axios.get(one);
-    const requestTwo = axios.get(two);
-    let resultado = { nombre: '', album: '', tipo: '', imagen: '' }
+    let resultado = { nombre: '', contenido: '', tipo: '', imagen: '', icon: '' }
     let busqueda = { resultado: [] }
-
-    axios.all([requestOne, requestTwo]).then(
-        axios.spread((...responses) => {
-            responses[0].data.results.map(function(info, index) {
-                resultado = { nombre: info.artistName, album: info.collectionName, tipo: info.kind, imagen: info.artworkUrl100 };
+    axios.get('https://itunes.apple.com/search?term=' + req.params.nombre + '&limit=10').then(response => {
+            response.data.results.map(function(info, index) {
+                resultado = { nombre: info.artistName, contenido: info.collectionName, tipo: info.kind, imagen: info.artworkUrl100, icon: 'https://w7.pngwing.com/pngs/1008/698/png-transparent-musical-note-illustration-apple-music-app-store-itunes-ios-11-share-fruit-nut-subscription-business-model-macrumors-thumbnail.png' };
                 busqueda.resultado.push(resultado);
             });
-            resultado = { nombre: responses[1].data.name, album: responses[1].data.language, tipo: responses[1].data.type, imagen: responses[1].data.image.medium };
-            busqueda.resultado.push(resultado);
             busqueda.resultado = busqueda.resultado.sort((a, b) => {
                 if (a.nombre > b.nombre) {
                     return 1;
@@ -30,11 +21,20 @@ app.get('/search/:nombre', function(req, res) {
 
                 return 0;
             })
-
             res.json({ busqueda });
-        })).catch(errors => {})
+        })
+        .catch(e => {
+
+        })
+    axios.get('https://api.tvmaze.com/singlesearch/shows?q=' + req.params.nombre).then(response2 => {
+            resultado = { nombre: response2.data.name, contenido: response2.data.language, tipo: response2.data.type, imagen: response2.data.image.medium, icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTvxRUx-NTtxbdqgfUxplgQSXUo0j4DD5c0dA&usqp=CAU' };
+            busqueda.resultado.push(resultado);
+        })
+        .catch(e => {
+
+        })
 
 });
-app.listen(5000, () => {
-    console.log("El servidor está inicializado en el puerto 5000");
+app.listen(4000, () => {
+    console.log("El servidor está inicializado en el puerto 4000");
 });
